3b07da6e96112f3cb9047bb5029d4ce9c1d35652
menu <lahkuhasunem@gmail.com>
2026-06-08 09:38:32 +0545

    Create, Retrieve, Update, Delete

--- rms/serializer.py
+++ rms/serializer.py
@@ -1,10 +1,35 @@
 from rest_framework import serializers
+from .models import *
 
+# function based
 class CategorySerializer(serializers.Serializer):
-    id = serializers.IntegerField()
+    id = serializers.IntegerField(read_only=True)
     name = serializers.CharField()
+    
+    def create(self, validated_data):
+        category = Category.objects.create(name = validated_data.get('name'))
+        return category
+    
+    def update(self, instance, validated_data):
+        instance.name = validated_data.get('name', instance.name)
+        instance.save()
+        return instance
     
 class TableSerializer(serializers.Serializer):
     number = serializers.IntegerField()
     capacity = serializers.IntegerField()
     is_available = serializers.BooleanField()
+    
+    def create(self, validated_data):
+        table = Table.objects.create(
+            number = validated_data.get('number'),
+            capacity = validated_data.get('capacity'),
+            is_available = validated_data.get('is_available'))
+        return table
+    
+    def update(self, instance, validated_data):
+        instance.number = validated_data.get('number', instance.number)
+        instance.capacity = validated_data.get('capacity', instance.capacity)
+        instance.is_available = validated_data.get('is_available', instance.is_available)
+        instance.save()
+        return instance
--- rms/urls.py
+++ rms/urls.py
@@ -3,5 +3,7 @@ from .views import *
 
 urlpatterns = [
     path('category/', category),
+    path('category/<id>/', category_detail),
     path('table/', table),
+    path('table/<id>/', table_detail),
 ]
